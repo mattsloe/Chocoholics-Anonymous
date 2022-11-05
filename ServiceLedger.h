bae651e2feb396e9b15d5e9224e6cd7c8301796f
Kobe Norcross <kobenorcross@gmail.com>
2022-11-05 14:00:35 -0700

    added additoinal members that should be added

--- ServiceLedger.h
+++ ServiceLedger.h
@@ -6,12 +6,13 @@
 class ServiceLedger {
     public:
         ServiceLedger();
-        ServiceRecord get_ServiceRecord(int pID);
+        ServiceRecord get_ServiceRecord(int mID);
         void create_ServiceRecord();
         void add_ServiceRecord();
 
     private:
     // data structure of Service Records
+    // probably hash table of service records ordered by their mID
 };
 
 #endif // SERVICE_LEDGER_H
