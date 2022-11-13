c49b19b609ac2de72ac4e81f27452b7aa51e96b2
Kobe Norcross <kobenorcross@gmail.com>
2022-11-12 17:01:31 -0800

    Edited Service Record Class to be simpler.

--- ServiceRecord.h
+++ ServiceRecord.h
@@ -1,11 +1,3 @@
-// FILE: ServiceRecord.cpp
-// Author: Kobe Norcross
-/*
-    ~ Decription ~
-    This file represents the service records that need to be kept
-    upon each provided service. 
-*/
-
 #ifndef SERVICE_RECORD_H
 #define SERVICE_RECORD_H
 
@@ -14,20 +6,18 @@
 
 class ServiceRecord {
     public:
-        ServiceRecord(); //- CLI prompt to initialize the service record
+
         ServiceRecord(std::string date, std::string sDate, int pID, int mID, int sID, std::string comments); //- initialized constructor
         ServiceRecord(std::string json); // - takes in a string json object and initializes Provider instance
+        // implement = operator overload
         void display(); //- displays to CLI
         void toJSON();  //- returns a string json object representing the instance of Provider
-        
-        // getters/setters
         void set_date(std::string date);
         void set_sDate(std::string sDate);
         void set_comments(std::string comments);
         void set_pID(int pID);
         void set_mID(int mID);
         void set_sID(int sID);
-
         std::string get_date();
         std::string get_sDate();
         std::string get_comments();
