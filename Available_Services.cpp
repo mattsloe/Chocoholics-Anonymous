8968fb2b87e91a70301ebb689a8295c4a0b2ee2b
Kobe Norcross <kobenorcross@gmail.com>
2022-11-21 19:45:16 -0800

    Combined the files and updating this class to be a central list instead of being a part of each Provider.

--- Available_Services.cpp
+++ Available_Services.cpp
@@ -1,6 +1,8 @@
 #include "Available_Services.hpp"
 using namespace std;
+using nlohmann::json = json;
 
+// ----------------------- Available Services Implementation ---------------------------
 Available_Services::~Available_Services() {
     for (auto it = services.begin(); it != services.end(); it++) {
         delete it->second;
@@ -65,4 +67,40 @@ bool Available_Services::get_service(unsigned long sID, Service *& service) {
     }
     service = val->second;
     return true;
+}
+
+// ----------------------- Service Implementation ---------------------------
+Service::Service() : name(), sID(0), fee(0.0) {}
+
+Service::Service(string name, unsigned long sID, double fee) : name(name), sID(sID), fee(fee) {}
+
+void Service::set_name(string name) {
+    this->name = name;
+}
+
+void Service::set_id(unsigned long sID) {
+    this->sID = sID;
+}
+
+void Service::set_fee(double fee) {
+    this->fee = fee;
+}
+
+std::string Service::get_name() {
+    return this->name;
+}
+
+void Service::display() {
+    cout << "Service Name: " << name << endl;
+    cout << "Service ID Number: " << sID << endl;
+    cout << "Service Fee: " << fee_output(this->fee) << endl;
+}
+
+string fee_output(double x) {
+    string out;
+    out += "$";
+    out += x;
+    set_precision(3);
+    //return ("%" + x);
+    return out;
 }
--- Available_Services.hpp
+++ Available_Services.hpp
@@ -1,7 +1,8 @@
 #ifndef AVAILABLE_SERVICES_HPP
 #define AVAILABLE_SERVICES_HPP
 
-#include "Service.hpp"
+#include "Service_Ledger.hpp"
+#include <iomanip>
 
 class Available_Services {
     public:
@@ -22,5 +23,28 @@ class Available_Services {
     private:
         std::map<int, Service*&> services;
 };
+
+class Service {
+    public:
+        Service();
+        Service(std::string, unsigned long, double);
+        void set_name(std::string);
+        void set_id(unsigned long);
+        void set_fee(double);
+        std::string get_name();
+        void display();
+
+        // writing to file
+        // write to json or string to return
+
+        // = operator overload
+
+    private:
+        std::string name;
+        unsigned long sID;
+        double fee;
+};
+
+std::string fee_output(double);
 
 #endif // AVAILABLE_SERVICES_HPP
