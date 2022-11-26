769e8f7283e7b12b5e95e791abb127a012eaa392
Kobe Norcross <kobenorcross@gmail.com>
2022-11-26 15:57:05 -0800

    Updating Service Ledger to follow suit with new code.

---
+++ Service_Ledger.cpp
@@ -0,0 +1,47 @@
+#include "Service_Ledger.hpp"
+using namespace std;
+using json = nlohmann::json;
+
+Service_Ledger::~Service_Ledger() {
+    for (auto it = ledger.begin(); it != ledger.end(); it++) {
+        delete it->second;
+        it->second = nullptr;
+    }
+}
+void Service_Ledger::new_transaction(Service_Record *&record) {
+    ledger.emplace(record->get_pID(), record);
+}
+
+void Service_Ledger::generate_APR() {
+    map<int, int> providers;
+    int total_providers = 0;
+    int total_services = 0;
+    double total_fee = 0.0;
+
+    for (auto it = ledger.begin(); it != ledger.end(); it++) {
+        if (providers.find(it->first) == providers.end()) continue;
+        providers.emplace(it->first, 0);
+        total_providers++;
+        for (auto pit = ledger.equal_range(it->first); pit != ledger.equal_range(it->first); ++pit) {
+            total_services++;
+            total_fee += dir.search(it->first);
+        }
+    }
+}
+
+void Service_Ledger::generate_EFT() {
+    map<int, int> providers;
+    string file_name = "EFTdata";
+    string date = "";
+    double pay_check = 0;
+    fstream f(file_name+date);
+    for (auto it = ledger.begin(); it != ledger.end(); it++) {
+        if (providers.find(it->first) == providers.end()) continue;
+        providers.emplace(it->first, 0);
+        pay_check = 0;
+        for (auto provider_transactions = ledger.equal_range(it->first); provider_transactions != ledger.equal_range(it->first); provider_transactions++) {
+            pay_check += dir.get_service_fee(provider_transactions->first);
+        }
+        // output the data to the file
+    }
+}
---
+++ Service_Ledger.hpp
@@ -0,0 +1,21 @@
+#ifndef SERVICE_LEDGER_HPP
+#define SERVICE_LEDGER_HPP
+
+#include "Service_Record.hpp"
+#include "json.hpp"
+#include <map>
+#include <utility>
+
+class Service_Ledger {
+    public:
+        ~Service_Ledger();
+        void new_transaction(Service_Record *&);
+        // update with 
+        void generate_APR();
+        void generate_EFT(); 
+
+    private:
+        std::multimap<int, Service_Record*&> ledger;
+};
+
+#endif // SERVICE_LEDGER_HPP
