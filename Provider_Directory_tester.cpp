9dbd374797731a39a6ee48a96db7ef20341f11dc
Kobe Norcross <kobenorcross@gmail.com>
2022-11-26 14:39:24 -0800

    Started drafting PD unit tests

--- Provider_Directory_tester.cpp
+++ Provider_Directory_tester.cpp
@@ -1,15 +1,14 @@
 #include "Provider_Directory_tester.hpp"
 
+using namespace std;
 
-void Provider_Directory_tester::test_Provider_Directory_class() {
+void PD_tester::test_Provider_Directory_class() {
     Provider_Directory *p = nullptr;
     cout << endl;
     cout << "------------------------------------------";
     cout << "PROVIDER DIRECTORY TESTS STARTING..." << endl;
 
     cout << "Find Valid test ";
-    (assert_test(default_constructor(p)));
-    cout << "Find valid test    ";
     (assert_test(find_valid(p)));
     cout << "Find invalid test    ";
     (assert_test(find_invalid(p)));
@@ -17,4 +16,17 @@ void Provider_Directory_tester::test_Provider_Directory_class() {
     cout << "PROVIDER DIRECTORY TESTS COMPLETE" << endl;
     cout << "------------------------------------------";
     cout << endl;
+}
+
+bool PD_tester::find_valid(Provider_Directory *p) {
+
+}
+
+bool PD_tester::find_invalid(Provider_Directory *p) {
+
+}
+
+
+static void assert_test(bool ret) {
+    cout << ((ret == false) ? "FAIL" : "PASS") << endl;
 }
--- Provider_Directory_tester.hpp
+++ Provider_Directory_tester.hpp
@@ -1,14 +1,14 @@
 #ifndef PROVIDER_DIRECTORY_TESTER_HPP
 #define PROVIDER_DIRECTORY_TESTER_HPP
 
-#include "Provider_Directory.hpp"
-
-class PD_Tester {
+class PD_tester {
     public:
         static void test_Provider_Directory_class();
         static void create_PD(Provider_Directory *&);
         static bool find_valid(Provider_Directory *);
         static bool find_invalid(Provider_Directory *);
 };
+
+static void assert_test(bool);
 
 #endif // PROVIDER_DIRECTORY_TESTER_HPP
