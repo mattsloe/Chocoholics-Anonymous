e00a5e9523c9a7bbc82f71f137fe2b85a6387722
Kobe Norcross <kobenorcross@gmail.com>
2022-11-26 14:10:01 -0800

    Continued drafting up unit tests for the Service Record class.

--- Service_Record_tester.cpp
+++ Service_Record_tester.cpp
@@ -1,33 +1,49 @@
-#include "Service_Record_tester.h"
+#include "Service_Record_tester.hpp"
 using namespace std;
 
-static void Service_Record::test_Service_Record_class() {
-    Server_Record * sr = nullptr;
+void Service_Record_tester::test_Service_Record_class() {
+    Service_Record * sr = nullptr;
     cout << endl;
-    cout << "------------------------------------------"
+    cout << "------------------------------------------";
     cout << "SERVICE RECORD TESTS STARTING..." << endl;
-    cout << "Default Constuctor test " <<  (assert_tests(default_constructor(sr)));
-    cout << "Copy Constuctor test    " <<  (assert_tests(copy_constructor(sr)));
-    cout << "JSON Constuctor test    " <<  (assert_tests(json_constructor(sr)));
-    cout << "Setting Date test       " << (assert_tests(setting_date(sr)));
-    cout << "Setting sID test        " << (assert_tests(setting_sID(sr)));
-    cout << "Setting mID test        " << (assert_tests(setting_mID(sr)));
-    cout << "Setting pID test        " << (assert_tests(setting_pID(sr)));
-    cout << "Setting comments test   " << (assert_tests(setting_comments(sr)));
+
+    cout << "Default Constuctor test ";
+    (assert_test(default_constructor(sr)));
+    cout << "Copy Constuctor test    ";
+    (assert_test(copy_constructor(sr)));
+    cout << "JSON Constuctor test    ";
+    (assert_test(json_constructor(sr)));
+    cout << "Setting Date test       "; 
+    (assert_test(setting_date(sr)));
+    cout << "Setting sID test        "; 
+    (assert_test(setting_sID(sr)));
+    cout << "Setting mID test        "; 
+    (assert_test(setting_mID(sr)));
+    cout << "Setting pID test        "; 
+    (assert_test(setting_pID(sr)));
+    cout << "Setting comments test   "; 
+    (assert_test(setting_comments(sr)));
+
     cout << "SERVICE RECORD TESTS COMPLETE" << endl;
-    cout << "------------------------------------------"
+    cout << "------------------------------------------";
     cout << endl;
 }
 
-static void create_SR(Service_Record *& s) {
+void Service_Record_tester::create_SR(Service_Record *&s) {
     s = new Service_Record();
+    // create json
 }
 
-static bool setting_date(test_Service_Record_class * sr) {
-    create_SR(sr);
+bool Service_Record_tester::default_constructor(Service_Record *s) {
+    s = new Service_Record();
+    return (s == false) ? false : true;
+}
+
+bool Service_Record_tester::setting_date(Service_Record *s) {
+    create_SR(s);
 
 }
 
-static void assert_tests(bool ret) {
+static void assert_test(bool ret) {
     cout << ((ret == false) ? "FAIL" : "PASS") << endl;
 }
--- Service_Record_tester.hpp
+++ Service_Record_tester.hpp
@@ -7,11 +7,22 @@ class Service_Record_tester {
     public:
         // Main Driver Call 
         static void test_Service_Record_class();
+        static void create_SR(Service_Record *&);
 
-        // tests
+        // constructor tests
         static bool default_constructor(Service_Record *);
         static bool copy_constructor(Service_Record *);
         static bool json_constructor(Service_Record *);
+
+        // data storing tests
+        static bool setting_date(Service_Record *);
+        static bool setting_sDate(Service_Record *);
+        static bool setting_sID(Service_Record *);
+        static bool setting_mID(Service_Record *);
+        static bool setting_pID(Service_Record *);
+        static bool setting_comments(Service_Record *);
 };
+
+static void assert_test(bool);
 
 #endif // SERVICE_RECORD_TESTER_HPP
