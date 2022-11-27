0766089d7660316228769c430f9f2c0d8e967924
Kobe Norcross <kobenorcross@gmail.com>
2022-11-27 14:56:01 -0800

    Finished code errors.

--- Service_Record.cpp
+++ Service_Record.cpp
@@ -1,6 +1,8 @@
 #include "Service_Record.hpp"
 using namespace std;
 
+Service_Record::Service_Record() : date(), sDate(), comments(), pID(), mID(), sID() {}
+
 Service_Record::Service_Record(const Service_Record & source) : date(source.date), sDate(source.sDate), comments(source.comments), pID(source.pID), mID(source.mID), sID(source.sID) {}
 
 Service_Record::Service_Record(string date, string sDate, string pID, string mID, string sID, string comments) : date(date), sDate(sDate), pID(pID), mID(mID), sID(sID), comments(comments) {}
@@ -125,11 +127,11 @@ void Service_Record::create_Service_Record() {
 
 string Service_Record::input_sDate() {
     string sDate;
-    sDate += get_part_date();
+    sDate += get_part_date(2);
     sDate += "-";
-    sDate += get_part_date();
+    sDate += get_part_date(2);
     sDate += "-";
-    sDate += get_year();
+    sDate += get_part_date(4);
     return sDate;
 }
 
@@ -162,6 +164,7 @@ static string get_part_date(int size) {
         else 
             valid = true;
     } 
+    return s;
 }
 
 static string input_ID(int size) {
