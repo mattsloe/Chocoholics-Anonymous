da7614ea095cb1af1387c56506555a7e51d155c4
Kobe Norcross <kobenorcross@gmail.com>
2022-11-27 21:04:08 -0800

    Added most of code coverage for service record.

--- Service_Record_tester.cpp
+++ Service_Record_tester.cpp
@@ -88,7 +88,36 @@ bool Service_Record_tester::copy_constructor(Service_Record *s) {
 }
 
 bool Service_Record_tester::json_constructor(Service_Record *s) {
+    bool compare = false;
+
     create_SR(s);
+
+    if (s->get_date() == test_date) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_sDate() == test_sDate)
+        compare = true;
+    else
+        compare = false;
+    if (s->get_pID() == test_pID) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_mID() == test_mID)
+        compare = true;
+    else
+        compare = false;
+    if (s->get_sID() == test_sID) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_comments() == test_comment) 
+        compare = true;
+    else
+        compare = false;
+
+    return compare;
 }
 
 bool Service_Record_tester::setting_date(Service_Record *s) {
@@ -103,6 +132,34 @@ bool Service_Record_tester::setting_sDate(Service_Record *s) {
     create_SR(s);
     s->set_sDate(date);
     return (s->get_sDate() == date) ? true : false; 
+}
+
+bool Service_Record_tester::setting_pID(Service_Record *s) {
+    string id = "123456789";
+    create_SR(s);
+    s->set_pID(id);
+    return (s->get_pID() == id) ? true : false;
+}
+
+bool Service_Record_tester::setting_mID(Service_Record *s) {
+    string id = "123456789";
+    create_SR(s);
+    s->set_pID(id);
+    return (s->get_mID() == id) ? true : false;
+}
+
+bool Service_Record_tester::setting_sID(Service_Record *s) {
+    string id = "123456";
+    create_SR(s);
+    s->set_sID(id);
+    return (s->get_sID() == id) ? true : false;
+}
+
+bool Service_Record_tester::setting_comments(Service_Record *s) {
+    string comment = "hi, what is good in the neighborhood?";
+    create_SR(s);
+    s->set_comments(comment);
+    return (s->get_comments() == comment) ? true : false;
 }
 
 static void assert_test(bool ret) {
