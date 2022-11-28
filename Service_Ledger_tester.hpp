85efeb78e46d1f81d824881870a183c7c43565b6
Kobe Norcross <kobenorcross@gmail.com>
2022-11-27 22:32:22 -0800

    Edited to include a create SL fxn

--- Service_Ledger_tester.hpp
+++ Service_Ledger_tester.hpp
@@ -3,14 +3,19 @@
 
 #include "Service_Ledger.hpp"
 
-class Service_Ledger_tester {
+// Service Ledger tester
+class SL_tester {
     public:
         static void test_Service_Ledger_class();
+        static void create_SL(Service_Ledger *&);
         
         // tests
-        static bool test_generate_EFT(Service_Ledger *);
-        static bool test_generate_APR(Service_Ledger *);
-        static bool test_new_transaction(Service_Ledger *);
+        static bool new_transaction_good_test(Service_Ledger *);
+        static bool new_transaction_bad_test(Service_Ledger *);
+        static bool generate_EFT_test(Service_Ledger *);
+        static bool generate_APR_test(Service_Ledger *);
 };
+
+static void assert_test(bool);
 
 #endif // SERVICE_LEDGER_TESTER_HPP
