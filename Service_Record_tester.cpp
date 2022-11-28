37258b354c84e582a2b969bb142ac36bccd9a1ac
Kobe Norcross <kobenorcross@gmail.com>
2022-11-27 17:35:45 -0800

    Finished copy constructor unit test.

--- Service_Record_tester.cpp
+++ Service_Record_tester.cpp
@@ -30,18 +30,75 @@ void Service_Record_tester::test_Service_Record_class() {
 }
 
 void Service_Record_tester::create_SR(Service_Record *&s) {
-    s = new Service_Record();
-    // create json
+    s = new Service_Record(j);
 }
 
 bool Service_Record_tester::default_constructor(Service_Record *s) {
     s = new Service_Record();
-    return (s == false) ? false : true;
+    return (s == nullptr) ? false : true;
+}
+
+bool Service_Record_tester::copy_constructor(Service_Record *s) {
+    bool compare = false;
+    Service_Record * test = nullptr;
+    string date = "123"
+    , sDate = "234"
+    , pID = "123456789"
+    , mID = "123456789"
+    , sID = "012345"
+    , comment = "hi world";
+    nlohmann::json j = {
+    {"date",date},
+    {"sDate",sDate},
+    {"pID",pID},
+    {"mID",mID},
+    {"sID",sID},
+    {"comments",comment}
+    };
+
+    create_SR(s);
+    create_SR(test);
+
+    if (s->get_date() == test->get_date()) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_sDate() == test->get_sDate()) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_pID() == test->get_pID()) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_mID() == test->get_mID()) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_sID() == test->get_sID()) 
+        compare = true;
+    else
+        compare = false;
+    if (s->get_comments() == test->get_comments()) 
+        compare = true;
+    else
+        compare = false;
+
+    return compare; 
 }
 
 bool Service_Record_tester::setting_date(Service_Record *s) {
+    string date = "08/17/2019 07:17:57";
     create_SR(s);
+    s->set_date(date);
+    return (s->get_date() == date) ? true : false; 
+}
 
+bool Service_Record_tester::setting_sDate(Service_Record *s) {
+    string date = "08/17/2019";
+    create_SR(s);
+    s->set_sDate(date);
+    return (s->get_sDate() == date) ? true : false; 
 }
 
 static void assert_test(bool ret) {
