6d069bd617eb35ca7f52609bddb445cccb4de0e7
Kobe Norcross <kobenorcross@gmail.com>
2022-11-27 17:23:36 -0800

    Added test data to test code.

--- Service_Record_tester.hpp
+++ Service_Record_tester.hpp
@@ -25,4 +25,22 @@ class Service_Record_tester {
 
 static void assert_test(bool);
 
+// test data 
+static std::string test_date = "07/17/2022 12:23:57";
+static std::string test_sDate = "07/17/2022";
+static std::string test_pID = "123456789";
+static std::string test_mID = "012345678";
+static std::string test_sID = "123456";
+static std::string test_comment = "Hello worlHello worlHello worlHello worlHello worlHello worlHello worlHello worlHello worlHello worl";
+    // 100 chars to test the capability
+
+nlohmann::json j = {
+    {"date",test_date},
+    {"sDate",test_sDate},
+    {"pID",test_pID},
+    {"mID",test_mID},
+    {"sID",test_sID},
+    {"comments",test_comment}
+};
+
 #endif // SERVICE_RECORD_TESTER_HPP
