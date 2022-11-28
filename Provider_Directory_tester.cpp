35eccca347500b4907fa92673810aee44a4eec91
Kobe Norcross <kobenorcross@gmail.com>
2022-11-27 21:41:36 -0800

    Added new tests and implemented file_constructor

--- Provider_Directory_tester.cpp
+++ Provider_Directory_tester.cpp
@@ -8,21 +8,43 @@ void PD_tester::test_Provider_Directory_class() {
     cout << "------------------------------------------";
     cout << "PROVIDER DIRECTORY TESTS STARTING..." << endl;
 
-    cout << "Find Valid test ";
-    (assert_test(find_valid(p)));
-    cout << "Find invalid test    ";
-    (assert_test(find_invalid(p)));
+    cout << "File Constructor test";
+    (assert_test(file_constructor(p)));
+    cout << "Validate Service Code (Success) test ";
+    (assert_test(validateID_success(p)));
+    cout << "Validate Service Code (Failure) test ";
+    (assert_test(validateID_fail(p)));
 
     cout << "PROVIDER DIRECTORY TESTS COMPLETE" << endl;
     cout << "------------------------------------------";
     cout << endl;
 }
 
-bool PD_tester::find_valid(Provider_Directory *p) {
-
+void PD_tester::create_PD(Provider_Directory *&p) {
+    string file_name = "assets/services.json";
+    p = new Provider_Directory(file_name);
 }
 
-bool PD_tester::find_invalid(Provider_Directory *p) {
+bool PD_tester::file_constructor(Provider_Directory *p) {
+    string id = "903295";
+    Service * service = nullptr;
+    create_PD(p);
+    p->get_service(id, service);
+
+    // data retrieved from line 21 of services.json
+    if (service->get_name() == "Okuma" 
+        && service->get_sID() == id 
+        && service->get_fee() == 770.72)
+        return true;
+
+    return false;
+}
+
+bool PD_tester::validateID_success(Provider_Directory *p) {
+    
+}
+
+bool PD_tester::validateID_fail(Provider_Directory *p) {
 
 }
 
--- Provider_Directory_tester.hpp
+++ Provider_Directory_tester.hpp
@@ -1,12 +1,16 @@
 #ifndef PROVIDER_DIRECTORY_TESTER_HPP
 #define PROVIDER_DIRECTORY_TESTER_HPP
 
+#include "Provider_Directory.hpp"
+
 class PD_tester {
     public:
         static void test_Provider_Directory_class();
         static void create_PD(Provider_Directory *&);
-        static bool find_valid(Provider_Directory *);
-        static bool find_invalid(Provider_Directory *);
+
+        static bool file_constructor(Provider_Directory *);
+        static bool validateID_success(Provider_Directory *);
+        static bool validateID_fail(Provider_Directory *);
 };
 
 static void assert_test(bool);
