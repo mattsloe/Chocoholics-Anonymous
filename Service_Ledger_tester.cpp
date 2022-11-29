b67800781dfb8a7054b522a9cfe96eae3f30242c
Kobe Norcross <kobenorcross@gmail.com>
2022-11-28 23:03:18 -0800

    Added more functionality and need to add Service_Ledger items for this to work.

--- Service_Ledger_tester.cpp
+++ Service_Ledger_tester.cpp
@@ -19,6 +19,28 @@ void SL_tester::test_Service_Ledger_class() {
     cout << "SERVICE LEDGER TESTS COMPLETE" << endl;
 }
 
+void SL_tester::create_SL(Service_Ledger *&s) {
+    s = new Service_Ledger();
+    Service_Record s1 = Service_Record(sr1);
+    Service_Record s2 = Service_Record(sr2);
+    Service_Record s3 = Service_Record(sr3);
+    s->new_transaction(s1);
+    s->new_transaction(s2);
+    s->new_transaction(s3);
+}
+
+bool SL_tester::new_transaction_good_test(Service_Ledger* s) {
+    Service_Record s1b, s1c;
+    create_SL(s);
+
+    s1b = Service_Record(sr1b);
+    s1c = Service_Record(sr1c);
+    s->new_transaction(s1b);
+    s->new_transaction(s1c);
+
+    
+}
+
 static void assert_test(bool ret) {
     cout << ((ret == false) ? "FAIL" : "PASS") << endl;
 }
