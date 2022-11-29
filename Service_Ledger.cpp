4a501d4440898c7fc357dbceb41e6a3b3bd4ef73
Kobe Norcross <kobenorcross@gmail.com>
2022-11-28 23:31:31 -0800

    Added PD instance and searching for vector error

--- Service_Ledger.cpp
+++ Service_Ledger.cpp
@@ -3,24 +3,23 @@ using namespace std;
 using json = nlohmann::json;
 
 void Service_Ledger::new_transaction(Service_Record &record) {
-    string id = record.get_pID();
+    auto new_record = Service_Record(record); // copy passed in record
+    string id = new_record.get_pID();
     auto check = this->ledger.find(id);
-    auto r = Service_Record(record); // copy passed in record
 
-    // append record to list when collision happens
-    if (check->first == id) {
-        ledger[id].push_back(r); 
+    // append record to list at the service code when collision happens
+    if (in_ledger(id)) {
+        ledger[id].push_back(new_record); 
         return;
     }
     
-    // create a new vector and append record 
+    // create a new vector and append record, then add it to the ledger
     vector<Service_Record> provider;
-    provider.push_back(r);
-    // add it to the ledger
-    ledger.emplace(record.get_pID(), provider);
+    provider.push_back(new_record);
+    ledger.emplace(id, provider);
 }
 
-void Service_Ledger::generate_APR() {
+void Service_Ledger::generate_APR(Provider_Directory *provider_directory) {
     map<string, int> providers;
     string sid = "", file_name = "AccountsPayableReport";
     int total_providers = 0, total_services = 0, num_consultations = 0;
@@ -30,6 +29,7 @@ void Service_Ledger::generate_APR() {
     file_name += current_date_time();
     file_name += ".txt";
     out.open(file_name);
+
     if (!out.is_open()) {
         cerr << "Unable to open file!!" << endl;
         exit(-17);
@@ -37,31 +37,25 @@ void Service_Ledger::generate_APR() {
 
     for (auto it = ledger.begin(); it != ledger.end(); it++) {
         sid = it->first;
-
         if (providers.find(sid) == providers.end()) continue;
         providers.emplace(sid, 0);
-
         total_providers++;
         num_consultations = 0;
-
         for (auto pit = ledger[sid].begin(); pit != ledger[sid].end(); pit++) {
-            total_fee += provider_directory.get_fee_d(sid);
+            total_fee += provider_directory->get_fee_d(sid);
             total_services++;
             num_consultations++;
         }
-
         // output to file
-        out << provider_directory.get_name(sid) << " (" << sid << ")\t" << "total consultations: " << to_string(num_consultations) << "," << "total fee: " << fee_output(total_fee) << endl;
-
+        out << provider_directory->get_name(sid) << " (" << sid << ")\t" << "total consultations: " << to_string(num_consultations) << "," << "total fee: " << fee_output(total_fee) << endl;
     }
 
     out << "Total number of Providers who provided services: " << to_string(total_providers) << endl;
     out << "Total number of Services provided: " << to_string(total_services);
-
     out.close();
 }
 
-void Service_Ledger::generate_EFT() {
+void Service_Ledger::generate_EFT(Provider_Directory *provider_directory) {
     map<string, int> providers;
     string file_name = "EFTdata", sid;
     double pay_check = 0;
@@ -70,6 +64,7 @@ void Service_Ledger::generate_EFT() {
     file_name += current_date_time();
     file_name += ".txt";
     out.open(file_name);
+    
     if (!out.is_open()) {
         cerr << "Unable to open file!!" << endl;
         exit(-17);
@@ -78,18 +73,24 @@ void Service_Ledger::generate_EFT() {
     for (auto it = ledger.begin(); it != ledger.end(); it++) {
 
         sid = it->first;
-
         if (providers.find(sid) == providers.end()) continue;
         providers.emplace(sid, 0);
         pay_check = 0;
 
         for (auto provider_transactions = ledger[sid].begin(); provider_transactions != ledger[sid].end(); provider_transactions++) {
-            pay_check += provider_directory.get_fee_d(sid);
+            pay_check += provider_directory->get_fee_d(sid);
         }
 
         // output the data to the file
-        out << provider_directory.get_name(sid) << " (" << sid << ")\t" << "Paycheck: " << to_string(pay_check) << endl;
-    }
+        out << provider_directory->get_name(sid) << " (" << sid << ")\t" << "Paycheck: " << to_string(pay_check) << endl;
 
+    }
     out.close();
+}
+
+bool Service_Ledger::in_ledger(string id) {
+    auto val = ledger.find(id);
+    if (val->first == id && val != ledger.end())
+        return true;
+    return false;
 }
