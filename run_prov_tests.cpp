e37b5cf3e9be1f99cab0f046f933fb298d4986c2
Ashton Sawyer <63769504+ashtonesawyer@users.noreply.github.com>
2022-12-02 18:59:17 -0800

    Delete run_prov_tests.cpp

--- run_prov_tests.cpp
+++
@@ -1,8 +0,0 @@
-#include "Provider_tester.hpp"
-#include "Provider_Database_tester.hpp"
-
-int main() {
-	Provider_tester::test_provider_class();
-	ProviderDB_tester::test_Provider_Database_class();
-	return 0;
-}
